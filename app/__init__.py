01b34d1ed1358055d181286bd255891f5ffe7a22
Fedor Nikolaev <fridrixnm@gmail.com>
2020-04-14 22:15:50 +0300

    More metrics

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
-
 from healthcheck import HealthCheck
+from prometheus_flask_exporter import PrometheusMetrics
+
 from .main import main as main_blueprint
 
+
 app = Flask(__name__)
+PrometheusMetrics(app)
+
+endpoints = (
+    '',
+    'health_check',
+)
 
 # Register routes:
 app.register_blueprint(main_blueprint)
-#app.register_blueprint(metrics_blueprint)
 
 health = HealthCheck()
 app.add_url_rule("/health_check", "healthcheck", view_func=lambda: health.run())
